8a00ce8d27f207ae63847cfd3e5a4d7c7467fbe8
Lingyu Zhou <zly@DebianPilot.home.net>
2018-09-05 15:02:31 -0400

    remove files

--- intervals.r
+++
@@ -1,8 +0,0 @@
-dyIntervals <-function(dygraph,data) {
-  dyPlugin(
-    dygraph = dygraph,
-    name = "Intervals",
-    path = "BCH_dygraph_plugins/intervals.js",
-    options=list(data=toJSON(data))
-  )
-}
