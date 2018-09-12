f450e70b61fe33cd4b65b5222a6f42e2bae350d6
Lingyu Zhou <zly@DebianPilot.home.net>
2018-09-12 00:44:03 -0400

    Bug fixs for utildiv plugin

--- utildiv.js
+++ utildiv.js
@@ -9,99 +9,224 @@ Dygraph.Plugins.UtilDiv = (function() {
   "use strict";
 
   /**
+   * Create divs on upper, right, and hover position with specified ids.
+   * The divs, if specified, will show when mouse hovers on the graph.
    * @constructor
    */
   var utildiv = function(options) {
     console.log({utildiv:options});
     // Get options
-    this.upper_left_ = document.createElement("div");
-    this.upper_right_ = null;
-    this.lower_left_ = null;
-    this.lower_right_ = null;
-    this.hover_ = document.createElement("div");
+    this.upper_ = (options.upper ) ? null : document.createElement("div");
+    this.right_ = (options.right ) ? null : document.createElement("div");
+    this.hover_ = (options.hover ) ? null : document.createElement("div");
     // Other variables
-    this.g=null;
+    this.g = null;
+    this.divOpacity_ = 0;
+    this.shownDivs_ = [];
+    this.animation_ = null;
     // Styling
-    this.upper_left_.style.height="30px";
-    this.upper_left_.style.width="20px";
-    this.upper_left_.style.position="absolute";
-    this.upper_left_.style.backgroundColor="#cccccc";
-    this.upper_left_.style.opacity=0.8;
-    this.upper_left_.style.borderRadius="2px";
-    this.upper_left_.style.visibility="hidden";
-    this.hover_.style.minHeight="100px";
-    this.hover_.style.width="200px";
-    this.hover_.style.position="fixed";
-    this.hover_.style.backgroundColor="#cccccc";
-    this.hover_.style.opacity=0.8;
-    this.hover_.style.borderRadius="2px";
-    this.hover_.style.visibility="hidden";
+    if (this.upper_ !== null) {
+      this.shownDivs_.push(this.upper_);
+      this.upper_.id=options.upper;
+      this.upper_.style.height="30px";
+      this.upper_.style.position="absolute";
+      this.upper_.style.top="-30px";
+      this.upper_.style.backgroundColor=this.divBG;
+      this.upper_.style.borderTopLeftRadius="2px";
+      this.upper_.style.borderTopRightRadius="2px";
+      this.upper_.style.visibility="hidden";
+    }
+    if (this.right_ !== null) {
+      this.shownDivs_.push(this.right_);
+      this.right_.id=options.right;
+      this.right_.style.width="30px";
+      this.right_.style.position="absolute";
+      this.right_.style.backgroundColor=this.divBG;
+      this.right_.style.borderTopRightRadius="2px";
+      this.right_.style.borderBottomRightRadius="2px";
+      this.right_.style.visibility="hidden";
+    }
+    if (this.hover_ !== null) {
+      this.shownDivs_.push(this.hover_);
+      this.hover_.id=options.hover;
+      this.hover_.style.maxWidth="200px";
+      this.hover_.style.position="fixed";
+      this.hover_.style.backgroundColor=this.divBG;
+      this.hover_.style.borderRadius="5px";
+      this.hover_.style.visibility="hidden";
+    }
   };
+  utildiv.prototype.divBG = "#C5B9AC"; //boston_warm_gray
+  utildiv.prototype.maxOpacity = 0.8;
   
+  /**
+   * Removes DOM, clear variables
+   */
   utildiv.prototype.destroy = function (){
-    //TODO
+    this.upper_.remove();
+    this.right_.remove();
+    this.hover_.remove();
+    this.upper_.null;
+    this.right_.null;
+    this.hover_.null;
+    this.g = null;
+    this.divOpacity_ = null;
+    this.shownDivs_ = null;
+    this.animation_ = null;
   };
   
   /**
-   * Methods
+   * For debug
    */
   utildiv.prototype.toString = function() {
     return "UtilDiv Plugin";
   };
-
+  
+  /**
+   * Show divs on mouseover
+   */
   utildiv.prototype.mouseover = function(ev) {
-    this.upper_left_.style.visibility="visible";
+    this.showAllDivs();
   };
   
+  /**
+   * Hide divs on mouseout
+   */
   utildiv.prototype.mouseout = function(ev) {
-    this.upper_left_.style.visibility="hidden";
+    this.hideAllDivs();
   };
-  
+
+  /**
+   * Update hover div position on mousemove
+   */
   utildiv.prototype.mousemove = function(ev) {
-    let x = ev.clientX-this.hover_.clientWidth/2;
-    let y = ev.clientY-this.hover_.clientHeight-10;
-    this.hover_.style.top=y+"px";
-    this.hover_.style.left=x+"px";
+    if (this.hover_){
+      let x = ev.clientX-this.hover_.clientWidth/2;
+      let y = ev.clientY-this.hover_.clientHeight-10;
+      this.hover_.style.top=y+"px";
+      this.hover_.style.left=x+"px";
+    }
   };
   
+  /**
+   * Add listener for custom events
+   */
   utildiv.prototype.activateCustomEventHandler = function(){
     let self = this;
-    this.g.graphDiv.addEventListener("mouseover",function(ev){
-      self.mouseover(ev);
-    });
-    this.g.graphDiv.addEventListener("mouseout",function(ev){
-      self.mouseout(ev);
-    });
-    this.g.graphDiv.addEventListener("mousemove",function(ev){
-      self.mousemove(ev);
-    });
+    if (this.shownDivs_.length !== 0) {
+      this.g.graphDiv.addEventListener("mouseover", function(ev){
+        self.mouseover(ev);
+      });
+      this.g.graphDiv.addEventListener("mouseout", function(ev){
+        self.mouseout(ev);
+      });
+    }
+    if (this.hover_) {
+      this.g.graphDiv.addEventListener("mousemove", function(ev){
+        self.mousemove(ev);
+      });
+    }
   };
 
-  utildiv.prototype.select = function(e){
-    this.hover_.style.visibility="visible";
+  /**
+   * We have to adjust the right div position when chart is redrawn
+   * (e.g. upon resize)
+   */
+  utildiv.prototype.didDrawChart = function(e){
+    if (this.right_) {
+      this.right_.style.left=this.g.width_+"px";
+    }
   }
 
-  utildiv.prototype.deselect = function(e){
-    this.hover_.style.visibility="hidden";
-  }
   /**
    * @param {Dygraph} g Graph instance.
    * @return {object.<string, function(ev)>} Mapping of event names to callbacks.
    */
   utildiv.prototype.activate = function(g) {
     this.g=g;
-    g.graphDiv.appendChild(this.upper_left_);
-    g.graphDiv.appendChild(this.hover_);
+    this.shownDivs_.forEach(function(div){
+      g.graphDiv.appendChild(div);
+    });
     this.activateCustomEventHandler();
     return {
-      select: this.select,
-      deselect: this.deselect,
+      didDrawChart: this.didDrawChart
     };
   };
 
-  /**
+  /********************
    * Helpers
+   ********************/
+  /**
+   * First call, timestamp is null:
+   * - cancel existing animation,
+   * - make divs visible
+   * - increase opacity
+   * - set up recusion
+   * Following call, timestamp is not null:
+   * - increase opacity
+   * - recusion
+   * Stop condition:
+   * - opacity max out
    */
+  utildiv.prototype.showAllDivs = function(timestamp=null) {
+    if (timestamp === null) {
+      if (this.animation_ !== null) {
+        window.cancelAnimationFrame(this.animation_);
+      }
+      this.shownDivs_.forEach(function(div) {
+        div.style.visibility = "visible";
+      });
+    }
+    if (this.divOpacity_ === this.maxOpacity_) {
+      this.animation_ = null;
+      return;
+    }
+    this.divOpacity_ = Math.min( this.maxOpacity,
+                                this.divOpacity_+0.1);
+    let self = this;
+    this.shownDivs_.forEach(function(div){
+      div.style.opacity = self.divOpacity_;
+    });
+    this.animation_ = window.requestAnimationFrame(function(timestamp) {
+      self.showAllDivs(timestamp);
+    });
+  };
+
+  /**
+   * First call, timestamp is null:
+   * - cancel existing animation,
+   * - decrease opacity
+   * - set up recusion
+   * Following call, timestamp is not null:
+   * - decrease opacity
+   * - recusion
+   * Stop condition:
+   * - opacity min out
+   * - toggle divs visibility
+   */
+  utildiv.prototype.hideAllDivs = function(timestamp=null) {
+    if (timestamp === null) {
+      if (this.animation_ !== null) {
+        window.cancelAnimationFrame(this.animation_);
+      }
+    }
+    if (this.divOpacity_ === 0) {
+      this.animation_ = null;
+      this.shownDivs_.forEach(function(div){
+        div.style.visibility = "hidden";
+      });
+      return;
+    }
+    this.divOpacity_ = Math.max( 0,
+                                this.divOpacity_-0.2);
+    let self = this;
+    this.shownDivs_.forEach(function(div){
+      div.style.opacity = self.divOpacity_;
+    });
+    this.animation_ = window.requestAnimationFrame(function(timestamp) {
+      self.hideAllDivs(timestamp);
+    });
+  };
   
   return utildiv;
 })();
--- utildiv.r
+++ utildiv.r
@@ -1,10 +1,9 @@
 dyUtilDiv <-function( dygraph,
-                      upper_left=NULL,upper_right=NULL,
-                      lower_left=NULL,lower_right=NULL,
-                      hover=NULL) {
+                      upper=NULL,right=NULL,hover=NULL) {
   dyPlugin(
     dygraph = dygraph,
     name = "UtilDiv",
-    path = "BCH_dygraph_plugins/utildiv.js"
+    path = "BCH_dygraph_plugins/utildiv.js",
+    options = list(upper = upper, right = right, hover = hover)
   )
 }
